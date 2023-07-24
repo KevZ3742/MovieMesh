d1b5ad45ad4c56afafaf940cb7546f68bbb5788f
KevZ3742 <kzhang3742@gmail.com>
2023-07-24 13:25:40 -0500

    set up html boiler plate

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return "home page"
+    return render_template("index.html")
