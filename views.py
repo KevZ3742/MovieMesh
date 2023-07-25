65483a71345362005f673320b27261c13f35ec4d
KevZ3742 <kzhang3742@gmail.com>
2023-07-25 09:05:43 -0500

    played around with getting and setting data from html with flask

--- views.py
+++ views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html")
+    test = request.args.get('query')
+    print(request.args)
+    return render_template("index.html", query=test)
