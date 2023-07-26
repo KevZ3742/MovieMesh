204de7de026df71f182cecf423142e4e60d935ab
KevZ3742 <kzhang3742@gmail.com>
2023-07-26 07:26:44 -0500

    created sample query dropdown

---
+++ static/scripts/script.js
@@ -0,0 +1,45 @@
+// Test data
+const data = [
+    "Apple",
+    "Banana",
+    "Cherry",
+    "Grapes",
+    "Lemon",
+    "Orange",
+    "Peach",
+    "Pear",
+    "Strawberry",
+];
+
+const searchInput = document.getElementById("search-input");
+const searchResults = document.getElementById("search-results");
+
+function updateResults() {
+    const query = searchInput.value.toLowerCase();
+    const filteredData = data.filter(item =>
+        item.toLowerCase().includes(query)
+    );
+
+    searchResults.innerHTML = "";
+
+    filteredData.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item;
+        searchResults.appendChild(li);
+    });
+
+    if (query === "") {
+        searchResults.style.display = "none";
+    } else {
+        searchResults.style.display = "block";
+    }
+}
+
+searchInput.addEventListener("input", updateResults);
+
+searchResults.addEventListener("click", event => {
+    if (event.target.tagName === "LI") {
+        searchInput.value = event.target.textContent;
+        searchResults.style.display = "none";
+    }
+});
--- views.py
+++ views.py
@@ -5,5 +5,4 @@ views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
     test = request.args.get('query')
-    print(request.args)
     return render_template("index.html", query=test)
