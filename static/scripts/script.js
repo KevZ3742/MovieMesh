1552f0c4068b32d64008b72058f937ffdd06e588
KevZ3742 <kzhang3742@gmail.com>
2023-07-28 15:40:31 -0500

    fixed some formatting

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -3,72 +3,72 @@ const searchResults = document.getElementById("search-results");
 let debounceTimer;
 
 async function fetchSearchResults(query) {
-    try {
-        const response = await fetch(`/api/search?query=${encodeURIComponent(query)}`);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        return [];
-    }
+  try {
+    const response = await fetch(`/api/search?query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
 }
 
 async function updateResults() {
-    const query = searchInput.value.toLowerCase();
+  const query = searchInput.value.toLowerCase();
 
-    searchResults.innerHTML = "";
+  searchResults.innerHTML = "";
 
-    if (query === "") {
-        searchResults.style.display = "none";
-        clearTimeout(debounceTimer);
-        return;
-    }
-
-    // Debounce
+  if (query === "") {
+    searchResults.style.display = "none";
     clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(async () => {
-        const searchResultsData = await fetchSearchResults(query);
+    return;
+  }
 
-        searchResultsData.forEach(item => {
-            let dataType;
-            let imgSrc;
+  // Debounce
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(async () => {
+    const searchResultsData = await fetchSearchResults(query);
 
-            if (item.media_type === "movie") {
-                dataType = "Movie";
-                imgSrc = `https://image.tmdb.org/t/p/w92${item.poster_path}`;
-            } else if (item.media_type === "tv") {
-                dataType = "TV Show";
-                imgSrc = `https://image.tmdb.org/t/p/w92${item.poster_path}`;
-            } else if (item.media_type === "person") {
-                dataType = "Actor";
-                imgSrc = `https://image.tmdb.org/t/p/w92${item.profile_path}`;
-            } else {
-                // Unsupported media type, skip
-                return;
-            }
+    searchResultsData.forEach(item => {
+      let dataType;
+      let imgSrc;
 
-            const li = document.createElement("li");
-            const img = document.createElement("img");
-            img.src = imgSrc;
-            li.appendChild(img);
+      if (item.media_type === "movie") {
+        dataType = "Movie";
+        imgSrc = `https://image.tmdb.org/t/p/w92${item.poster_path}`;
+      } else if (item.media_type === "tv") {
+        dataType = "TV Show";
+        imgSrc = `https://image.tmdb.org/t/p/w92${item.poster_path}`;
+      } else if (item.media_type === "person") {
+        dataType = "Actor";
+        imgSrc = `https://image.tmdb.org/t/p/w92${item.profile_path}`;
+      } else {
+        // Unsupported media type, skip
+        return;
+      }
 
-            const span = document.createElement("span");
-            span.textContent = dataType === "Actor" ? item.name : item.title || item.name;
-            li.appendChild(span);
+      const li = document.createElement("li");
+      const img = document.createElement("img");
+      img.src = imgSrc;
+      li.appendChild(img);
 
-            searchResults.appendChild(li);
-        });
+      const span = document.createElement("span");
+      span.textContent = dataType === "Actor" ? item.name : item.title || item.name;
+      li.appendChild(span);
 
-        searchResults.style.display = "block";
-    }, 300);
+      searchResults.appendChild(li);
+    });
+
+    searchResults.style.display = "block";
+  }, 300);
 }
 
 searchInput.addEventListener("input", updateResults);
 
 searchResults.addEventListener("click", event => {
-    if (event.target.tagName === "LI") {
-        const selectedText = event.target.textContent.trim();
-        searchInput.value = selectedText;
-        searchResults.style.display = "none";
-    }
+  if (event.target.tagName === "LI") {
+    const selectedText = event.target.textContent.trim();
+    searchInput.value = selectedText;
+    searchResults.style.display = "none";
+  }
 });
