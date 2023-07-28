af092c778bc8589a9b8c438e12ee11c50d502b4e
KevZ3742 <kzhang3742@gmail.com>
2023-07-27 23:45:43 -0500

    added search result dropdown

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,56 +1,79 @@
-// Test data
-const data = [
-    {
-        imgSrc: "temp1.jpg",
-        text: "Apple",
-    },
-    {
-        imgSrc: "temp2.jpg",
-        text: "Banana",
-    },
-    {
-        imgSrc: "temp3.jpg",
-        text: "Cherry",
-    },
-];
+async function fetchSearchResults(query) {
+  const apiKey = "API_KEY";
+  const apiUrl = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${encodeURIComponent(
+    query
+  )}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+}
 
 const searchInput = document.getElementById("query");
 const searchResults = document.getElementById("search-results");
+let debounceTimer;
 
-function updateResults() {
-    const query = searchInput.value.toLowerCase();
-    const filteredData = data.filter(item =>
-        item.text.toLowerCase().includes(query)
-    );
+async function updateResults() {
+  const query = searchInput.value.toLowerCase();
 
-    searchResults.innerHTML = "";
+  searchResults.innerHTML = "";
 
-    filteredData.forEach(item => {
-        const li = document.createElement("li");
-        const img = document.createElement("img");
-        img.src = item.imgSrc;
-        li.appendChild(img);
+  if (query === "") {
+    searchResults.style.display = "none";
+    clearTimeout(debounceTimer);
+    return;
+  }
 
-        const span = document.createElement("span");
-        span.textContent = item.text;
-        li.appendChild(span);
+  // Debounce
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(async () => {
+    const searchResultsData = await fetchSearchResults(query);
 
-        searchResults.appendChild(li);
+    searchResultsData.forEach(item => {
+      let dataType;
+      let imgSrc;
+
+      if (item.media_type === "movie") {
+        dataType = "Movie";
+        imgSrc = `https://image.tmdb.org/t/p/w92${item.poster_path}`;
+      } else if (item.media_type === "tv") {
+        dataType = "TV Show";
+        imgSrc = `https://image.tmdb.org/t/p/w92${item.poster_path}`;
+      } else if (item.media_type === "person") {
+        dataType = "Actor";
+        imgSrc = `https://image.tmdb.org/t/p/w92${item.profile_path}`;
+      } else {
+        // Unsupported media type, skip
+        return;
+      }
+
+      const li = document.createElement("li");
+      const img = document.createElement("img");
+      img.src = imgSrc;
+      li.appendChild(img);
+
+      const span = document.createElement("span");
+      span.textContent = dataType === "Actor" ? item.name : item.title || item.name;
+      li.appendChild(span);
+
+      searchResults.appendChild(li);
     });
 
-    if (query === "") {
-        searchResults.style.display = "none";
-    } else {
-        searchResults.style.display = "block";
-    }
+    searchResults.style.display = "block";
+  }, 300);
 }
 
 searchInput.addEventListener("input", updateResults);
 
 searchResults.addEventListener("click", event => {
-    if (event.target.tagName === "LI") {
-        const selectedText = event.target.textContent.trim();
-        searchInput.value = selectedText;
-        searchResults.style.display = "none";
-    }
-});  
+  if (event.target.tagName === "LI") {
+    const selectedText = event.target.textContent.trim();
+    searchInput.value = selectedText;
+    searchResults.style.display = "none";
+  }
+});
