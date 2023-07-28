a2f9df324b290c44175fe88be315d6480510b404
KevZ3742 <kzhang3742@gmail.com>
2023-07-28 15:20:54 -0500

    made api stuff server side

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,79 +1,74 @@
-async function fetchSearchResults(query) {
-  const apiKey = "API_KEY";
-  const apiUrl = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${encodeURIComponent(
-    query
-  )}`;
-
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return [];
-  }
-}
-
 const searchInput = document.getElementById("query");
 const searchResults = document.getElementById("search-results");
 let debounceTimer;
 
+async function fetchSearchResults(query) {
+    try {
+        const response = await fetch(`/api/search?query=${encodeURIComponent(query)}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return [];
+    }
+}
+
 async function updateResults() {
-  const query = searchInput.value.toLowerCase();
+    const query = searchInput.value.toLowerCase();
 
-  searchResults.innerHTML = "";
+    searchResults.innerHTML = "";
 
-  if (query === "") {
-    searchResults.style.display = "none";
-    clearTimeout(debounceTimer);
-    return;
-  }
-
-  // Debounce
-  clearTimeout(debounceTimer);
-  debounceTimer = setTimeout(async () => {
-    const searchResultsData = await fetchSearchResults(query);
-
-    searchResultsData.forEach(item => {
-      let dataType;
-      let imgSrc;
-
-      if (item.media_type === "movie") {
-        dataType = "Movie";
-        imgSrc = `https://image.tmdb.org/t/p/w92${item.poster_path}`;
-      } else if (item.media_type === "tv") {
-        dataType = "TV Show";
-        imgSrc = `https://image.tmdb.org/t/p/w92${item.poster_path}`;
-      } else if (item.media_type === "person") {
-        dataType = "Actor";
-        imgSrc = `https://image.tmdb.org/t/p/w92${item.profile_path}`;
-      } else {
-        // Unsupported media type, skip
+    if (query === "") {
+        searchResults.style.display = "none";
+        clearTimeout(debounceTimer);
         return;
-      }
+    }
 
-      const li = document.createElement("li");
-      const img = document.createElement("img");
-      img.src = imgSrc;
-      li.appendChild(img);
+    // Debounce
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(async () => {
+        const searchResultsData = await fetchSearchResults(query);
 
-      const span = document.createElement("span");
-      span.textContent = dataType === "Actor" ? item.name : item.title || item.name;
-      li.appendChild(span);
+        searchResultsData.forEach(item => {
+            let dataType;
+            let imgSrc;
 
-      searchResults.appendChild(li);
-    });
+            if (item.media_type === "movie") {
+                dataType = "Movie";
+                imgSrc = `https://image.tmdb.org/t/p/w92${item.poster_path}`;
+            } else if (item.media_type === "tv") {
+                dataType = "TV Show";
+                imgSrc = `https://image.tmdb.org/t/p/w92${item.poster_path}`;
+            } else if (item.media_type === "person") {
+                dataType = "Actor";
+                imgSrc = `https://image.tmdb.org/t/p/w92${item.profile_path}`;
+            } else {
+                // Unsupported media type, skip
+                return;
+            }
 
-    searchResults.style.display = "block";
-  }, 300);
+            const li = document.createElement("li");
+            const img = document.createElement("img");
+            img.src = imgSrc;
+            li.appendChild(img);
+
+            const span = document.createElement("span");
+            span.textContent = dataType === "Actor" ? item.name : item.title || item.name;
+            li.appendChild(span);
+
+            searchResults.appendChild(li);
+        });
+
+        searchResults.style.display = "block";
+    }, 300);
 }
 
 searchInput.addEventListener("input", updateResults);
 
 searchResults.addEventListener("click", event => {
-  if (event.target.tagName === "LI") {
-    const selectedText = event.target.textContent.trim();
-    searchInput.value = selectedText;
-    searchResults.style.display = "none";
-  }
+    if (event.target.tagName === "LI") {
+        const selectedText = event.target.textContent.trim();
+        searchInput.value = selectedText;
+        searchResults.style.display = "none";
+    }
 });
--- views.py
+++ views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
+import requests
+import os
+from utils import *
 
 views = Blueprint(__name__, "views")
 
@@ -6,3 +9,29 @@ views = Blueprint(__name__, "views")
 def home():
     test = request.args.get('query')
     return render_template("index.html", query=test)
+
+# MovieMesh search API
+@views.route('/api/search')
+def fetch_search_results():
+    query = request.args.get('query')
+    
+    if not query:
+        return jsonify([])
+
+    url = os.getenv("BASE_URL") + "search/multi"
+    params = {
+        "api_key": os.getenv("API_KEY"),
+        "query": query,
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json().get("results", [])
+        
+        allowedKeys = ["media_type", "poster_path", "title", "name", "profile_path"]
+        
+        return jsonify(dataCleaner(data, allowedKeys))
+    except requests.RequestException as e:
+        print("Error fetching data:", e)
+        return jsonify([])
